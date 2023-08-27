dbad78c9048324f8e9be3772af3561fa2633e212
ataher83 <141751330+ataher83@users.noreply.github.com>
2023-08-27 10:20:45 -0700

    Add files via upload

--- IslamicProject/urls.py
+++ IslamicProject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import islam 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',islam, name="islam"), 
 ]
